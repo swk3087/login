8cd26b6066346c4b3777670829072cc8b6870efa
swk3087 <89524975+swk3087@users.noreply.github.com>
2023-01-12 17:08:38 +0900

    Add files via upload

---
+++ 7.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>로그인 구현</title>
+</head>
+<body>
+    <script>
+        password = prompt("비밀번호");
+        if(password == 11411){
+            document.write("안녕하세요. 주인님");
+        } else {
+            document.write("뉘신지?");
+        }
+    </script>
+</body>
+</html>
